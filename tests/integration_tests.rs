e3fca7a4af6f8d225881b8fcf7d5347097af0303
Nick Nad <nadolskinick@gmail.com>
2025-11-03 07:08:14 +0100

    integration tests include new subscribe, unsubscribe, verify

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,13 +2,16 @@ mod tests {
     const HEX_KEY: &str = "327da54c32bbda6f1b56c2e248620d31324b6bf5664fc31ab4d455f38787a5fa";
 
     use axum::{body::Body, extract::Request, http::StatusCode};
+    use email_serv::http;
     use email_serv::http::ApiContext;
+    use email_serv::http::SubscriptionEmail;
     use email_serv::http::create_router;
+    use mime::Mime;
     use parking_lot::Mutex;
     use std::collections::HashMap;
     use std::sync::Arc;
     use std::u8;
-    use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+    use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready
 
     #[tokio::test]
     async fn test_fallback() {
@@ -57,16 +60,65 @@ mod tests {
     async fn test_unsubscribe() {
         let mut array = [0u8; 32];
         hex::decode_to_slice(HEX_KEY, &mut array as &mut [u8]).unwrap();
+        let email = String::from("test@email.com");
+        let mut hasher = blake3::Hasher::new_keyed(&array);
+        hasher.update(email.clone().as_bytes());
+        let hash = hasher.finalize().to_hex().to_string();
+        let mut hashmap = HashMap::new();
+        hashmap.insert(
+            hash.clone(),
+            SubscriptionEmail {
+                email: email,
+                is_verified: false,
+            },
+        );
         let context = ApiContext {
-            emails: Arc::new(Mutex::new(HashMap::new())),
+            emails: Arc::new(Mutex::new(hashmap)),
             blake3_key: array,
         };
         let app = create_router(context);
         let response = app
             .oneshot(
                 Request::builder()
-                    .method(axum::http::Method::POST)
-                    .uri("/api/unsubscribe")
+                    .method(axum::http::Method::GET)
+                    .uri(format!("/api/unsubscribe?token={}", hash))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_verify() {
+        let mut array = [0u8; 32];
+        let email = String::from("test@email.com");
+
+        hex::decode_to_slice(HEX_KEY, &mut array as &mut [u8]).unwrap();
+        let mut hasher = blake3::Hasher::new_keyed(&array);
+        hasher.update(email.clone().as_bytes());
+        let hash = hasher.finalize().to_hex().to_string();
+        let mut hashmap = HashMap::new();
+        hashmap.insert(
+            hash.clone(),
+            SubscriptionEmail {
+                email: email,
+                is_verified: false,
+            },
+        );
+        let context = ApiContext {
+            emails: Arc::new(Mutex::new(hashmap)),
+            blake3_key: array,
+        };
+
+        let app = create_router(context);
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(axum::http::Method::GET)
+                    .uri(format!("/api/verify?token={}", hash))
                     .body(Body::empty())
                     .unwrap(),
             )
@@ -88,9 +140,13 @@ mod tests {
         let response = app
             .oneshot(
                 Request::builder()
-                    .method(axum::http::Method::GET)
+                    .method(axum::http::Method::POST)
                     .uri("/api/subscribe")
-                    .body(Body::empty())
+                    .header(
+                        axum::http::header::CONTENT_TYPE,
+                        mime::APPLICATION_JSON.as_ref(),
+                    )
+                    .body(Body::from(String::from("{\"email\": \"test@email.de\"}")))
                     .unwrap(),
             )
             .await
