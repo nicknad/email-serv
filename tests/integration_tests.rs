c5841661b59d4461371840be02b5b8abb0face08
Nick Nad <nadolskinick@gmail.com>
2025-11-06 04:00:12 +0100

    added rate limiting test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -7,11 +7,12 @@ mod tests {
     use email_serv::http::SubscriptionEmail;
     use email_serv::http::create_router;
     use http_body_util::Full;
-    use mime::Mime;
     use parking_lot::Mutex;
     use std::collections::HashMap;
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
     use std::sync::Arc;
-    use std::u8;
+    use std::time::Duration;
+    use tokio::net::TcpListener;
     use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready
 
     #[tokio::test]
@@ -191,5 +192,68 @@ mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::PAYLOAD_TOO_LARGE);
+    }
+
+    #[tokio::test]
+    async fn test_rate_limit() {
+        let mut array = [0u8; 32];
+        hex::decode_to_slice(HEX_KEY, &mut array as &mut [u8]).unwrap();
+        let context = ApiContext {
+            emails: Arc::new(Mutex::new(HashMap::new())),
+            blake3_key: array,
+        };
+        let app = create_router(context);
+        let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 5000);
+        let listener = tokio::net::TcpListener::bind(socket).await.unwrap();
+
+        let server = axum::serve(listener, app);
+        let _handle = tokio::spawn(async move {
+            server.await.expect("Test server failed to run");
+        });
+        wait_for_server_ready(&socket).await;
+
+        let http_client = reqwest::Client::new();
+        for i in 0..7 {
+            let result = http_client
+                .post("http://127.0.0.1:5000/api/subscribe")
+                .header("X-Forwarded-For", "127.0.0.1")
+                .header(
+                    axum::http::header::CONTENT_TYPE,
+                    mime::APPLICATION_JSON.as_ref(),
+                )
+                .body("{\"email\": \"test@email.de\"}")
+                .send()
+                .await;
+
+            assert!(result.is_ok());
+
+            if i < 5 {
+                assert_eq!(result.unwrap().status(), StatusCode::OK);
+            } else {
+                assert_ne!(result.unwrap().status(), StatusCode::OK);
+            }
+        }
+    }
+
+    async fn wait_for_server_ready(addr: &SocketAddr) {
+        let mut attempts = 0;
+        const MAX_ATTEMPTS: u8 = 10;
+        const DELAY: Duration = Duration::from_millis(50);
+
+        loop {
+            match TcpStream::connect(addr) {
+                Ok(_) => {
+                    // Connection successful, server is likely up
+                    return;
+                }
+                Err(_) => {
+                    attempts += 1;
+                    if attempts >= MAX_ATTEMPTS {
+                        panic!("Server failed to start after {} attempts.", MAX_ATTEMPTS);
+                    }
+                    tokio::time::sleep(DELAY).await;
+                }
+            }
+        }
     }
 }
